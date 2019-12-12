09e9468bfc26c59fcc64403ca96fe563fd0a3739
Yoad Gantz <yoad08@gmail.com>
2019-12-12 19:39:29 +0200

    end day 2 - footer included

--- js/savedMemesController.js
+++ js/savedMemesController.js
@@ -3,12 +3,16 @@
 function renderSavedMemes() {
     let elSavedMemes = document.querySelector('.saved-memes');
     let savedMemes = getSavedMemes();
-    let imgsUrls = savedMemes.map(function (savedMeme) {
-        let imgHTML = `<div class="saved-meme-img"><img class="meme-img" src="${savedMeme}"/></div>`        
-        return imgHTML;
-    });
-    elSavedMemes.innerHTML = imgsUrls.join('');
+    console.log(savedMemes);
     
+    if (savedMemes.length) {
+        let imgsUrls = savedMemes.map(function (savedMeme) {
+            let imgHTML = `<div class="saved-meme-img"><img class="meme-img" src="${savedMeme}"/></div>`
+            return imgHTML;
+        });
+        elSavedMemes.innerHTML = imgsUrls.join('');
+    } else { elSavedMemes.innerHTML = `<h2> There are no saved memes now, go to gallery and create some dank memes </h2>`}
+
 }
 
 function onMemesTab(elOpt) {
